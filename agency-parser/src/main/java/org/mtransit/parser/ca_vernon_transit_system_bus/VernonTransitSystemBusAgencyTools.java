e2b8df83475c53a4ad7227e61c94d43e6a09b1b1
Mathieu Méa <mathieu.mea@gmail.com>
2024-06-04 20:38:16 -0400

    Migrate to new flatten git repo

---
+++ agency-parser/src/main/java/org/mtransit/parser/ca_vernon_transit_system_bus/VernonTransitSystemBusAgencyTools.java
@@ -0,0 +1,163 @@
+package org.mtransit.parser.ca_vernon_transit_system_bus;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.mtransit.commons.CleanUtils;
+import org.mtransit.parser.DefaultAgencyTools;
+import org.mtransit.parser.MTLog;
+import org.mtransit.parser.gtfs.data.GRoute;
+import org.mtransit.parser.mt.data.MAgency;
+
+import java.util.regex.Pattern;
+
+import static org.mtransit.commons.StringUtils.EMPTY;
+
+// https://www.bctransit.com/open-data
+// The North Okanagan Region consists of
+// Shuswap and Vernon.
+public class VernonTransitSystemBusAgencyTools extends DefaultAgencyTools {
+
+	public static void main(@NotNull String[] args) {
+		new VernonTransitSystemBusAgencyTools().start(args);
+	}
+
+	@Override
+	public boolean defaultExcludeEnabled() {
+		return true;
+	}
+
+	@NotNull
+	@Override
+	public String getAgencyName() {
+		return "Vernon Regional TS";
+	}
+
+	@NotNull
+	@Override
+	public Integer getAgencyRouteType() {
+		return MAgency.ROUTE_TYPE_BUS;
+	}
+
+	@Override
+	public boolean excludeRoute(@NotNull GRoute gRoute) {
+		final int rsn = Integer.parseInt(gRoute.getRouteShortName());
+		if (rsn > 20 && rsn < 60) {
+			return EXCLUDE; // part of Shuswap TS
+		}
+		return super.excludeRoute(gRoute);
+	}
+
+	@Override
+	public boolean defaultRouteIdEnabled() {
+		return true;
+	}
+
+	@Override
+	public boolean useRouteShortNameForRouteId() {
+		return false; // route ID used by GTFS RT
+	}
+
+	@Override
+	public @Nullable String getRouteIdCleanupRegex() {
+		return "\\-[A-Z]+$";
+	}
+
+	@Override
+	public boolean defaultAgencyColorEnabled() {
+		return true;
+	}
+
+	private static final String AGENCY_COLOR_GREEN = "34B233";// GREEN (from PDF Corporate Graphic Standards)
+	private static final String AGENCY_COLOR_BLUE = "002C77"; // BLUE (from PDF Corporate Graphic Standards)
+
+	private static final String AGENCY_COLOR = AGENCY_COLOR_GREEN;
+
+	@NotNull
+	@Override
+	public String getAgencyColor() {
+		return AGENCY_COLOR;
+	}
+
+	@SuppressWarnings("DuplicateBranchesInSwitch")
+	@Nullable
+	@Override
+	public String provideMissingRouteColor(@NotNull GRoute gRoute) {
+		switch (gRoute.getRouteShortName()) {
+		// @formatter:off
+		case "1": return "004B8E";
+		case "2": return "8AC641";
+		case "3": return "F68C1F";
+		case "4": return "8E0C3A";
+		case "5": return "E81D89";
+		case "6": return "01AEF0";
+		case "7": return "00AB4F";
+		case "8": return "B3AB7D";
+		case "9": return "E170AA";
+		case "11": return "FCAF18";
+		case "21": return AGENCY_COLOR_BLUE;
+		case "22": return AGENCY_COLOR_BLUE;
+		case "23": return AGENCY_COLOR_BLUE;
+		case "24": return AGENCY_COLOR_BLUE;
+		case "25": return AGENCY_COLOR_BLUE;
+		case "41": return AGENCY_COLOR_BLUE;
+		case "42": return AGENCY_COLOR_BLUE;
+		case "43": return AGENCY_COLOR_BLUE;
+		case "60": return "A7439B";
+		case "61": return "B3B828";
+		case "90": return "AB5B38";
+		// @formatter:on
+		}
+		throw new MTLog.Fatal("Unexpected route color for %s!", gRoute);
+	}
+
+	@Override
+	public boolean defaultRouteLongNameEnabled() {
+		return true;
+	}
+
+	@Override
+	public boolean directionSplitterEnabled(long routeId) {
+		return true;
+	}
+
+	@Override
+	public boolean directionOverrideId(long routeId) {
+		if (routeId == 1L) {
+			return true; // merge trips because 2 directions overlap
+		}
+		return super.directionOverrideId(routeId);
+	}
+
+	@Override
+	public boolean directionFinderEnabled() {
+		return true;
+	}
+
+	private static final Pattern STARTS_W_RLN_DASH_ = Pattern.compile("^.+- ", Pattern.CASE_INSENSITIVE);
+
+	private static final Pattern ENDS_WITH_DASH_ = Pattern.compile("-$", Pattern.CASE_INSENSITIVE);
+
+	@NotNull
+	@Override
+	public String cleanTripHeadsign(@NotNull String tripHeadsign) {
+		tripHeadsign = CleanUtils.keepToAndRemoveVia(tripHeadsign);
+		tripHeadsign = STARTS_W_RLN_DASH_.matcher(tripHeadsign).replaceAll(EMPTY);
+		tripHeadsign = ENDS_WITH_DASH_.matcher(tripHeadsign).replaceAll(EMPTY);
+		tripHeadsign = CleanUtils.CLEAN_AT.matcher(tripHeadsign).replaceAll(CleanUtils.CLEAN_AT_REPLACEMENT);
+		tripHeadsign = CleanUtils.CLEAN_AND.matcher(tripHeadsign).replaceAll(CleanUtils.CLEAN_AND_REPLACEMENT);
+		tripHeadsign = CleanUtils.cleanStreetTypes(tripHeadsign);
+		tripHeadsign = CleanUtils.cleanNumbers(tripHeadsign);
+		return CleanUtils.cleanLabel(tripHeadsign);
+	}
+
+	@NotNull
+	@Override
+	public String cleanStopName(@NotNull String gStopName) {
+		gStopName = CleanUtils.CLEAN_AT.matcher(gStopName).replaceAll(CleanUtils.CLEAN_AT_REPLACEMENT);
+		gStopName = CleanUtils.CLEAN_AND.matcher(gStopName).replaceAll(CleanUtils.CLEAN_AND_REPLACEMENT);
+		gStopName = CleanUtils.cleanBounds(gStopName);
+		gStopName = CleanUtils.cleanNumbers(gStopName);
+		gStopName = CleanUtils.cleanStreetTypes(gStopName);
+		return CleanUtils.cleanLabel(gStopName);
+	}
+}
